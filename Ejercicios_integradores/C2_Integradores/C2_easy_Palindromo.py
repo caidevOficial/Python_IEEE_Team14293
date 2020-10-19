70bdd8b3bca8bf2cc7a03b54ab182c0afc69904f
Facu Falcone <falcone.facu@gmail.com>
2020-10-19 00:22:13 -0300

    C2 Integradores

---
+++ Ejercicios_integradores/C2_Integradores/C2_easy_Palindromo.py
@@ -0,0 +1,14 @@
+# Definir una función que detecte si una palabra es un palíndromo y devuelve True o False.
+# Ejemplos:
+# palindromo( "python" ) => False
+# palindromo( "reconocer" ) => True
+# palindromo( "Neuquén" ) => False
+
+def esPalindromo(cadena):
+    cadena1 = cadena[::-1].lower()
+    if(cadena1 == str(cadena).lower()):
+        return True
+    return False
+
+x = esPalindromo("Neuquen")
+print(x)
