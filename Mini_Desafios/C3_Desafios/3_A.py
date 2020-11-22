0734b1ed799d9ce9cff15f0a8857494886083f46
CaidevOficial <falcone.facu@gmail.com>
2020-11-22 18:15:39 -0300

    Mini Desafio 3 corregido.

--- Mini_Desafios/C3_Desafios/3_A.py
+++ Mini_Desafios/C3_Desafios/3_A.py
@@ -6,7 +6,7 @@ import pandas as pd
 rd = pd.read_excel("Datos.xlsx")
 
 def aprobadosMatematica():
-    aprobados = rd[rd["Matematica"]>4]
+    aprobados = rd[rd["Matematica"]>=4]
     promedios = (aprobados["Matematica"] + aprobados["Quimica"] + aprobados["Fisica"])/3
     print(promedios)
 
