5c021e9f61ad5c60267c4200829bc4ff6c16fb54
CaidevOficial <falcone.facu@gmail.com>
2020-11-22 14:38:47 -0300

    Desafio 3

---
+++ Mini_Desafios/C3_Desafios/3_A.py
@@ -0,0 +1,14 @@
+# Obtener el promedio general sólo para aquellos alumnos 
+# que aprobaron Matematica.
+
+import pandas as pd 
+
+rd = pd.read_excel("Datos.xlsx")
+
+def aprobadosMatematica():
+    aprobados = rd[rd["Matematica"]>4]
+    promedios = (aprobados["Matematica"] + aprobados["Quimica"] + aprobados["Fisica"])/3
+    print(promedios)
+
+# Test
+aprobadosMatematica()
